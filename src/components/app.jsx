c29ff3978b28e8110a0dbdbe98545643ba2503be
Philip Krause <philip@Philips-MacBook-Air.local>
2023-09-04 11:30:19 +0200

    initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header';
+import Footer from './footer';
+import Card from './card';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Card />
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+    return (
+        <div className="note">
+            <h1 >HUHU</h1>
+            <p>Was da los?</p>
+        </div>
+    );
+}
+
+export default Card;
